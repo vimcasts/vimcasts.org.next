6326a142e02f9a86ebe94d1dfd5d71dbcd738979
Drew Neil <andrew.jr.neil@gmail.com>
2021-01-22 15:31:31 +0000

    wip  - start sketching type-guard functions

--- lib/api.ts
+++ lib/api.ts
@@ -2,6 +2,7 @@ import fs from 'fs'
 import { join } from 'path'
 import matter from 'gray-matter'
 import PostType, { GeneralPost } from '../types/post'
+import EpisodeType from '../types/episode'
 
 const postsDirectory = join(process.cwd(), '_posts')
 const episodesDirectory = join(process.cwd(), '_episodes')
@@ -14,25 +15,44 @@ export function getEpisodeSlugs() {
   return fs.readdirSync(episodesDirectory)
 }
 
+function isGeneralPost(input: unknown): input is GeneralPost {
+  return typeof input === "object" && input !== null && "slug" in input && "title" in input && "date" in input
+}
+
 export function getPostBySlug(slug: string, fields: string[] = []): PostType {
-  const data = getItemBySlug(slug, fields, postsDirectory)
+  const {data, realSlug, content} = getItemBySlug(slug, fields, postsDirectory)
+  if (!isGeneralPost(data)) {
+    throw new Error("The frontmatter for slug:" + slug + " is invalid")
+  }
+
   return {
-    ...data, _type: "post"
+    ...data, _type: "post", slug: realSlug, content
   }
 }
 
-export function getEpisodeBySlug(slug: string, fields: string[] = []) {
-  return getItemBySlug(slug, fields, episodesDirectory)
+function isEpisode(input: unknown): input is EpisodeType {
+  return typeof input === "object" && input !== null && "slug" in input && "title" in input && "date" in input
 }
 
-export function getItemBySlug(slug: string, fields: string[] = [], directory: string) {
+export function getEpisodeBySlug(slug: string, fields: string[] = []) {
+  const { data, realSlug, content} = getItemBySlug(slug, fields, episodesDirectory)
+  if (!isEpisode(data)) {
+    throw new Error("The frontmatter for slug: " + slug + " is invalid")
+  }
+}
+
+type Item = {
+  data: unknown;
+  realSlug: string;
+  content: string;
+}
+export function getItemBySlug(slug: string, fields: string[] = [], directory: string): Item {
   const realSlug = slug.replace(/\.md$/, '')
   const fullPath = join(directory, `${realSlug}.md`)
   const fileContents = fs.readFileSync(fullPath, 'utf8')
-  const { data, content } = matter(fileContents)
+  const { data, content }: { data: unknown, content: string} = matter(fileContents)
 
-  
-  return {...data, slug: realSlug, content: content,};
+  return {data, realSlug: realSlug, content: content,};
 }
 
 export function getAllPosts(fields: string[] = []) {
